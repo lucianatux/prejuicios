c6ff0712d88d20c3cecf5e60fd3585a30a0243c3
lucianatux <lucianatux@yahoo.com.ar>
2024-07-09 12:43:22 -0300

    modular

--- script.js
+++ script.js
@@ -1,69 +1,76 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const tutorialStep = document.getElementById("tutorial-step");
+  const contentDiv = document.getElementById("content-div");
+  const prejudicesSelect = document.getElementById("prejudices-select");
+  const endButton = document.getElementById("end-button");
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const tutorialStep1 = document.getElementById('tutorial-step-1');
-    const tutorialStep2 = document.getElementById('tutorial-step-2');
-    const tutorialStep3 = document.getElementById('tutorial-step-3');
-    const contentDiv = document.getElementById('content-div');
-    const prejudicesSelect = document.getElementById('prejudices-select');
-    const thoughtsDiv = document.getElementById('thoughts-div');
-    const endButton = document.getElementById('end-button');
-  
-    let currentThought = null; // Variable para almacenar el pensamiento actual
-  
-    function showElement(element) {
-      element.style.display = 'block';
-      setTimeout(() => {
-        element.classList.add('show');
-        element.classList.remove('fade');
-      }, 10); // Slight delay to ensure the transition is applied
-    }
-  
-    function hideElement(element) {
-      element.classList.add('fade');
-      element.classList.remove('show');
-      setTimeout(() => {
-        element.style.display = 'none';
-      }, 1000); // Coincide con la duración de la transición CSS
-    }
-  
-    // Step 1: Show the first tutorial step
-    showElement(tutorialStep1);
+  const steps = [
+    "Esta experiencia interactiva te mostrará cómo nuestros prejuicios pueden cambiar nuestra interpretación de las acciones de los demás.",
+    "Selecciona las distintas creencias, prejuicios y observa como cambian tus diálogos mentales y tu percepción de la realidad",
+    "¿Notaste cómo tu percepción de la historia cambió según tus prejuicios?<br><br>Esta experiencia nos recuerda que debemos ser conscientes de cómo nuestras opiniones sobre las personas pueden distorsionar nuestra interpretación de sus acciones."
+  ];
+
+  let currentThought = null; // Variable para almacenar el pensamiento actual
+
+  function showElement(element) {
+    element.style.display = "block";
     setTimeout(() => {
-      hideElement(tutorialStep1);
-      setTimeout(() => {
-        showElement(tutorialStep2);
-        showElement(contentDiv);
-      }, 1000); // Coincide con la duración de la transición CSS
-    }, 5000); // Adjust the duration as needed
+      element.classList.add("unfade");
+      element.classList.remove("fade");
+    }, 10); // Pequeño retraso para asegurar que la transición se aplique
+  }
+
+  function hideElement(element) {
+    element.classList.add("fade");
+    element.classList.remove("unfade");
+    setTimeout(() => {
+      element.style.display = "none"; // Después de la transición, oculta el elemento
+    }, 1000); // Espera el tiempo de la transición (1s en este caso)
+  }
+
+
+  function updateTutorialStep(index) {
+    hideElement(tutorialStep);
+    setTimeout(() => {
+      tutorialStep.innerHTML = steps[index];
+      showElement(tutorialStep);
+    }, 1000); // Ajusta la duración según sea necesario
+  }
   
-    prejudicesSelect.addEventListener('change', function() {
-      const selectedValue = prejudicesSelect.value;
-      const selectedThought = document.getElementById(selectedValue);
+  // Step 1: Show the first tutorial step
+  updateTutorialStep(0);
   
-      // Hide the current thought if there is one
-      if (currentThought) {
-        hideElement(currentThought);
-      }
+  setTimeout(() => {
+    updateTutorialStep(1);
+    showElement(contentDiv);
+  }, 5000); // Ajusta la duración según sea necesario
   
-      // Show the new thought
-      if (selectedThought) {
-        setTimeout(() => {
-          showElement(selectedThought);
-          currentThought = selectedThought; // Update the current thought
-        }, 1000); // Coincide con la duración de la transición CSS
-      }
-    });
-  
-    endButton.addEventListener('click', function() {
-      showElement(tutorialStep3);
-      
-        hideElement(tutorialStep2);
-      
-    });
-  
-    // Show the end button after selecting a thought
-    prejudicesSelect.addEventListener('change', function() {
-      showElement(endButton);
-    });
+  endButton.addEventListener("click", function () {
+    updateTutorialStep(2);
   });
   
+
+
+  prejudicesSelect.addEventListener("change", function () {
+    const selectedValue = prejudicesSelect.value;
+    const selectedThought = document.getElementById(selectedValue);
+
+    // Hide the current thought if there is one
+    if (currentThought) {
+      hideElement(currentThought);
+    }
+
+    // Show the new thought
+    if (selectedThought) {
+      setTimeout(() => {
+        showElement(selectedThought);
+        currentThought = selectedThought; // Update the current thought
+      }, 1000); // Coincide con la duración de la transición CSS
+    }
+  });
+
+  // Show the end button after selecting a thought
+  prejudicesSelect.addEventListener("change", function () {
+    showElement(endButton);
+  });
+});
